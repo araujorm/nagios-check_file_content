c569c1efffc5f07dfd907c4c841d0c9cbab41ac5
Rodrigo Araujo <roa@eurotux.com>
2023-07-21 15:51:36 +0100

    Version 1.0.0
    A partial rewrite of the plugin. Now uses the Monitoring::Plugin module
    to parse the options, support range threshold format and provide
    perfdata.
    
    The new -c and -w options fully support range format, while -n is kept
    for backwards compatibility (but considered deprecated and discouraged
    from being used in new setups).
    
    Some structural and code formatting changes were perfored in this
    version to better suit this fork's author preferences.

--- README.md
+++ README.md
@@ -1,26 +1,47 @@
 nagios-check_file_content
 =========================
 
-Nagios file parser check
+Nagios/Icinga plugin to check file contents
 
 Usage
 -----
 ```
-Usage : check_file_content.pl -f file -i include [-e exclude] [-n lines_number] [-m max_search] [-h]
+Usage : check_file_content.pl -f file -i include [-e exclude] [-c lines_number_range] [-m max_search] [--help]
 
-Options :
- -f
-	Full path to file to analyze (mandatory)
- -n
-	Number of lines to find (default is 1)
- -m
-	Maximum number of lines to search on (default is all)
- -i
-	Include pattern (mandatory, can be repeated multiple times)
- -e
-	Exclude pattern (can be repeated multiple times)
+ -?, --usage
+   Print usage information
  -h, --help
-	Print this help screen
+   Print detailed help screen
+ -V, --version
+   Print version information
+ --extra-opts=[section][@file]
+   Read options from an ini file. See https://www.monitoring-plugins.org/doc/extra-opts.html
+   for usage and examples.
+ -f=STRING
+   Full path to file to analyze (mandatory)
+ -c=STRING
+   Critical threshold for the number of lines to find, in RANGE format (see link below).
+   Default is "1:" which means "at least 1".
+ -w=STRING
+   Warning threshold for the number of lines to find, in RANGE format (see link below).
+   Defaults to none (no warning).
+ -n=INTEGER
+   (DEPRECATED) Number of lines to find (in integer format, for compatibility with older versions or
+   variants of this plugin). Usage of this option is discouraged in new setups.
+   Using this option with a N value is equivalent as using -c N: and it cannot be used at the same
+   time as -w or -c.
+ -m=INTEGER
+   Maximum number of lines to search on (default is all).
+ -i=STRING
+   Include pattern (mandatory, can be repeated multiple times).
+ -e=STRING
+   Exclude pattern (can be repeated multiple times).
+ -t, --timeout=INTEGER
+   Seconds before plugin times out (default: 15)
+ -v, --verbose
+   Show details for command-line debugging (can repeat up to 3 times)
 
 Example : check_file_content.pl -f /etc/passwd -i 0 -e root -n 5
+
+See https://www.monitoring-plugins.org/doc/guidelines.html#THRESHOLDFORMAT for the threshold format.
 ```
--- check_file_content.pl
+++ check_file_content.pl
@@ -1,177 +1,173 @@
 #!/usr/bin/perl -w
 #===============================================================================
-#
-#         FILE:  check_file_content.pl
-#
-#        USAGE:  ./check_file_content.pl
-#
-#  DESCRIPTION:  Nagios plugin to check file content
-#
-#      OPTIONS:  ---
-# REQUIREMENTS:  ---
-#         BUGS:  ---
-#        NOTES:  ---
-#       AUTHOR:  Pierre Mavro (), pierre@mavro.fr
-#                Rodrigo Araujo (version >= 0.2)
-#      COMPANY:
-#      VERSION:  0.2
-#      CREATED:  10/05/2010 09:25:56
-#                07/2023 (version 0.2)
-#     REVISION:  ---
+#       AUTHOR:  Pierre Mavro (), pierre@mavro.fr (original version 0.1)
+#                Rodrigo Araujo (version >= 1.0.0)
 #===============================================================================
 
 use warnings;
 use strict;
-use Getopt::Long;
+use Monitoring::Plugin;
+my $version = '1.0.0';
 
-my %RETCODES = ('OK' => 0, 'WARNING' => 1, 'CRITICAL' => 2, 'UNKNOWN' => 3);
 
-# Help
-sub help
-{
-	print "Usage : check_file_content.pl -f file -i include [-e exclude] [-n lines_number] [-m max_search] [-h]\n\n";
-	print "Options :\n";
-	print " -f\n\tFull path to file to analyze (mandatory)\n";
-	print " -n\n\tNumber of lines to find (default is 1)\n";
-	print " -m\n\tMaximum number of lines to search on (default is all)\n";
-	print " -i\n\tInclude pattern (mandatory, can be repeated multiple times)\n";
-	print " -e\n\tExclude pattern (can be repeated multiple times)\n";
-	print " -h, --help\n\tPrint this help screen\n";
-	print "\nExample : check_file_content.pl -f /etc/passwd -i 0 -e root -n 5\n";
-	exit $RETCODES{"UNKNOWN"};
+## Monitoring plugin data
+my $license = << 'EOLICENSE';
+Nagios/Icinga plugin to check file contents
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+EOLICENSE
+
+my $extra_help = << 'EOH';
+
+Example : check_file_content.pl -f /etc/passwd -i 0 -e root -n 5
+
+See https://www.monitoring-plugins.org/doc/guidelines.html#THRESHOLDFORMAT for the threshold format.
+EOH
+
+my $np = Monitoring::Plugin->new(
+	version => $version,
+	usage => "Usage : %s -f file -i include [-e exclude] [-c lines_number_range] [-m max_search] [--help]",
+	extra => $extra_help,
+	license => $license,
+);
+
+
+## Monitoring plugin arguments
+$np->add_arg(
+	spec => "f=s",
+	help => "Full path to file to analyze (mandatory)",
+	required => 1,
+);
+$np->add_arg(
+	spec => "c=s",
+	help => "Critical threshold for the number of lines to find, in RANGE format (see link below).\n".
+		"   Default is \"1:\" which means \"at least 1\".",
+	# default has to be set after some othe checks below, because of the possibility of the deprecated -n option being used instead
+);
+$np->add_arg(
+	spec => "w=s",
+	help => "Warning threshold for the number of lines to find, in RANGE format (see link below).\n".
+		"   Defaults to none (no warning).",
+);
+$np->add_arg(
+	spec => "n=i",
+	help => "(DEPRECATED) Number of lines to find (in integer format, for compatibility with older versions or\n".
+		"   variants of this plugin). Usage of this option is discouraged in new setups.\n".
+		"   Using this option with a N value is equivalent as using -c N: and it cannot be used at the same\n".
+		"   time as -w or -c.",
+);
+$np->add_arg(
+	spec => "m=i",
+	help => "Maximum number of lines to search on (default is all).",
+);
+$np->add_arg(
+	spec => "i=s@",
+	help => "Include pattern (mandatory, can be repeated multiple times).",
+	required => 1,
+);
+$np->add_arg(
+	spec => "e=s@",
+	help => "Exclude pattern (can be repeated multiple times).",
+);
+$np->getopts;
+
+my $file = $np->opts->get('f');
+my $crit = $np->opts->get('c');
+my $warn = $np->opts->get('w');
+my $num = $np->opts->get('n');
+my $maxl = $np->opts->get('m');
+my $qr_include = check_compile_regexps($np->opts->get('i'));
+my $qr_exclude = check_compile_regexps($np->opts->get('e'));
+
+if (defined($num)) {
+	# deprecated option, for compatibility purposes
+	if (defined($crit) || defined($warn)) {
+		$np->plugin_die("Option -n cannot be used at the same time as -c or -w");
+	}
+	if ($num < 1) {
+		$np->plugin_die("Option -n minimum value is 1");
+	}
+	$crit = "$num:";
+}
+elsif (!defined($crit)) {
+	# for the above test to work, we need to provide the critical default here
+	$crit = "1:";
 }
 
-sub check_compile_regexps
-{
+if (defined($maxl) && $maxl < 1) {
+	$np->plugin_die("Maximum number of lines to search on should be at least 1");
+}
+
+if (!open(FILER, '<', $file)) {
+	$np->plugin_die("Can't open $file: $!");
+}
+
+## Actual check
+my $i = 0;
+my $l = 0;
+while(<FILER>) {
+	if (defined($maxl)) {
+		$l++;
+		if ($l > $maxl) {
+			last;
+		}
+	}
+	chomp($_);
+	my $line = $_;
+	my $found = 0;
+
+	# Should match
+	foreach (@$qr_include) {
+		if ($line =~ $_) {
+			$found=1;
+			last;
+		}
+	}
+
+	# Shouldn't match
+	foreach (@$qr_exclude) {
+		if ($line =~ $_) {
+			$found=0;
+			last;
+		}
+	}
+
+	$i++ if ($found == 1);
+}
+close(FILER);
+
+my $code = $np->check_threshold(
+	check => $i,
+	warning => $warn,
+	critical => $crit,
+);
+$np->add_perfdata(
+	label => 'Matches',
+	value => $i,
+	min => 0,
+	warning => $warn,
+	critical => $crit,
+);
+$np->plugin_exit($code, "$file ($i match" . ( $i == 1 ? '' : 'es' ) . ' found' . ( defined($maxl) ? ' in the first ' . ( $maxl > 1 ? "$maxl lines" : 'line' ) : '' ) . ')');
+
+
+## Helper sub(s)
+sub check_compile_regexps {
 	my $list = shift;
 
 	my @qr_list;
-	foreach my $r (@$list)
-	{
-		my $qr = eval { qr/$r/ };
-		if ($@)
-		{
-			print "Invalid regex \"$r\": $@";
-			exit $RETCODES{"UNKNOWN"};
+	if (defined($list)) {
+		foreach my $r (@$list) {
+			my $qr = eval { qr/$r/ };
+			if ($@) {
+				$np->plugin_die("Invalid regex \"$r\": $@");
+			}
+			push @qr_list, $qr;
 		}
-		push @qr_list, $qr;
 	}
 
 	return \@qr_list;
 }
-
-sub check_args
-{
-	help if !@ARGV;
-
-	my ($file,@include,@exclude,$maxlines);
-	my $num=1;
-
-	# Set options
-	 GetOptions(
-		"help|h" => \&help,
-		"f=s"    => \$file,
-		"i=s"    => \@include,
-		"e=s"    => \@exclude,
-		"n=i"    => \$num,
-		"m=i"    => \$maxlines,
-	);
-
-	unless (defined($file) and (@include))
-	{
-	        &help;
-	}
-
-	unless ($num >= 1)
-	{
-		print "Minimum number of lines to find is 1\n";
-		exit $RETCODES{"UNKNOWN"};
-	}
-
-	if (defined($maxlines) && $maxlines < 1)
-	{
-		print "Maximum number of lines to search on should be at least 1\n";
-		exit $RETCODES{"UNKNOWN"};
-	}
-
-	my $qr_include = check_compile_regexps(\@include);
-	my $qr_exclude = check_compile_regexps(\@exclude);
-
-        check_soft($file,$num,$maxlines,$qr_include,$qr_exclude);
-}
-
-sub check_soft
-{
-	my $file=shift;
-	my $num=shift;
-	my $maxlines=shift;
-	my $qr_include=shift;
-	my $qr_exclude=shift;
-	my $i=0;
-
-	if (!open(FILER, '<', $file))
-	{
-		print "Can't open $file: $!\n";
-		exit $RETCODES{"UNKNOWN"};
-	}
-
-	my $l=0;
-	while(<FILER>)
-	{
-		if (defined($maxlines))
-		{
-			$l++;
-			if ($l > $maxlines)
-			{
-				last;
-			}
-		}
-		chomp($_);
-		my $line=$_;
-		my $found=0;
-
-		# Should match
-		foreach (@$qr_include)
-		{
-			if ($line =~ $_)
-			{
-				$found=1;
-				last;
-			}
-		}
-
-		# Shouldn't match
-		foreach (@$qr_exclude)
-		{
-			if ($line =~ $_)
-			{
-				$found=0;
-				last;
-			}
-		}
-
-		$i++ if ($found == 1);
-	}
-	close(FILER);
-
-	if ($i > 0)
-	{
-		if ($i >= $num)
-		{
-			print "OK for $file ($i found)\n";
-			exit $RETCODES{"OK"};
-		}
-		else
-		{
-			print "FAILED on $file. Found only $i on $num\n";
-			exit $RETCODES{"CRITICAL"};
-		}
-	}
-	else
-	{
-		print "FAILED on $file\n";
-		exit $RETCODES{"CRITICAL"};
-	}
-}
-
-check_args;
